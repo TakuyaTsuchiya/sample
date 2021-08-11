7dbb565ec44b2e65f4acf28da6abe9eeb8f16e0f
Takuya Tsuchiya <takuya21@gmail.com>
2021-08-11 19:06:44 +0900

    developed memo編集機能と削除機能

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -4,11 +4,33 @@ class MemosController < ApplicationController
   end
 
   def new
-
+    @categories = Category.all
   end
 
   def create
-    Memo.create(title:params["memos"]["title"], body:params["memos"]["body"])
+    Memo.create(title:params["memos"]["title"],
+                body:params["memos"]["body"],
+                category_id:params['memos']['category_id'])
+    redirect_to '/'
+  end
+
+  def destroy
+    memo = Memo.find(params["id"])
+    memo.destroy
+    redirect_to '/'
+  end
+
+  def edit
+    @memo = Memo.find(params['id'])
+    @categories = Category.all
+  end
+
+  def update
+    memo = Memo.find(params['id'])
+    memo.title = params['memos']['title']
+    memo.body = params['memos']['body']
+    memo.category_id = params['memos']['category_id']
+    memo.save
     redirect_to '/'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   post '/create', to: 'memos#create'
   get '/sum_input', to: 'sums#sum_input'
   post '/addition', to: 'sums#addition'
+  delete '/memos/:id', to: 'memos#destroy'
+  get '/memos/:id/edit', to: 'memos#edit'
+  patch '/memos/:id', to: 'memos#update'
 end
